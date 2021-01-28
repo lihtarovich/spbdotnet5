3298ba933a29ca730556bba27c9c9bead511873c
Kirill Likhtarovich <lihtarovich@gmail.com>
2021-01-28 17:51:23 +0100

    Fixed Dockerfile

--- scripts/publishToLinux.sh
+++ scripts/publishToLinux.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 
 ssh -t kirill@spbdotnet.local 'mkdir -p /spbdotnet/docker/net5/'
-scp ../Dockerfile kirill@spbdotnet.local:/spbdotnet/docker/net5/
-
+#scp ../Dockerfile kirill@spbdotnet.local:/spbdotnet/docker/net5/
+rsync -ar --progress ../Dockerfile* kirill@spbdotnet.local:/spbdotnet/docker/net5/
 
